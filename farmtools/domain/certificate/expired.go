45bc72566e08ddc0ef447f9eda38908e2d07a135
auho <auho@163.com>
2024-03-13 14:19:32 +0800

    domain

--- farmtools/domain/certificate/expired.go
+++ farmtools/domain/certificate/expired.go
@@ -9,14 +9,20 @@ import (
 
 func QuickCheckExpired(domains []string, alarmDays int) {
 
+	var errorDomains []string
 	expiredDomains := make(map[string]int)
 
 	var nowTime = time.Now()
 	for _, domain := range domains {
-		conn, _ := tls.Dial("tcp", domain+":443", nil)
+		conn, err := tls.Dial("tcp", domain+":443", nil)
+		if err != nil {
+			errorDomains = append(errorDomains, domain)
+			continue
+		}
+
 		cert := conn.ConnectionState().PeerCertificates[0]
 
-		color.Black(fmt.Sprintf("Subject: %v\n", cert.Subject))
+		color.Black(fmt.Sprintf("Domain: %s, %v\n", domain, cert.Subject))
 		color.Black(fmt.Sprintf("Expired: %s => %s\n", cert.NotBefore.Format(time.DateTime), cert.NotAfter.Format(time.DateTime)))
 		color.Magenta(fmt.Sprintf("Expired Days: %f 天", cert.NotAfter.Sub(nowTime).Seconds()/86400))
 
@@ -32,6 +38,13 @@ func QuickCheckExpired(domains []string, alarmDays int) {
 
 		fmt.Println()
 	}
+
+	fmt.Println("Error Domain:")
+	for _, domain := range errorDomains {
+		color.Red(domain)
+	}
+
+	fmt.Println()
 
 	fmt.Println("Approaching Expired:")
 	for _d, _i := range expiredDomains {
